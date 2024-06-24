2672f13f187d394dd0f4af65a4a0451dbc5b67b9
dmanslick <dvvshah@gmail.com>
2024-06-24 19:23:03 -0400

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,140 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from 'expo-router'
+import React, { useState } from 'react'
+import { TabBarIcon } from '@/components/navigation/TabBarIcon'
+import { Colors } from '@/constants/Colors'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { Button, Image, StyleSheet, View, Text, TextInput, Pressable, Dimensions } from 'react-native'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme()
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [phoneNum, setPhoneNum] = useState('')
+    const [hasRide, setHasRide] = useState(true)
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    const studentLogin = () => {
+        setLoggedIn(true)
+    }
+
+    const nonStudentLogin = () => {
+        if (!(phoneNum === '')) {
+            setLoggedIn(true)
+        }
+    }
+
+    if (loggedIn) {
+        return (
+            <>
+                {hasRide && (
+                    <Pressable style={{ alignSelf: 'stretch' }} onPress={() => setHasRide(false)}>
+                        <View style={{ height: 32, backgroundColor: 'limegreen' }} />
+                    </Pressable>
+                )}
+                <Tabs
+                    screenOptions={{
+                        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                        headerShown: false,
+                    }}>
+                    <Tabs.Screen
+                        name='calendar'
+                        options={{
+                            title: '',
+                            tabBarIcon: ({ color }) => (
+                                <TabBarIcon name='calendar-outline' color={color} />
+                            ),
+                        }}
+                    />
+                    <Tabs.Screen
+                        name='index'
+                        options={{
+                            title: '',
+                            tabBarIcon: ({ color, focused }) => (
+                                <TabBarIcon name='home-outline' color={color} />
+                            ),
+                        }}
+                    />
+                    <Tabs.Screen
+                        name='settings'
+                        options={{
+                            title: '',
+                            tabBarIcon: ({ color }) => (
+                                <TabBarIcon name='settings-outline' color={color} />
+                            ),
+                        }}
+                    />
+                </Tabs>
+            </>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image
+                        style={styles.roamImage}
+                        source={require('@/assets/images/logo.jpg')}
+                    />
+                    <Text style={styles.roamImageText}>Roam</Text>
+                </View>
+                <View style={styles.students}>
+                    <Text style={styles.helperText}>Students</Text>
+                    <Button title='Log In' onPress={studentLogin} />
+                </View>
+                <View style={styles.nonStudents}>
+                    <Text style={styles.helperText}>Non-Students</Text>
+                    <TextInput
+                        placeholder='Phone Number'
+                        style={styles.phoneNumberInput}
+                        placeholderTextColor='#C0C0C0'
+                        onChange={text => setPhoneNum(text as unknown as string)}
+                    />
+                    <Button
+                        title='Enter'
+                        onPress={nonStudentLogin}
+                    />
+                </View>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        alignSelf: 'center',
+        marginTop: 48,
+        maxWidth: 300,
+        marginHorizontal: 'auto'
+    },
+
+    roamImage: {
+        aspectRatio: 1,
+        width: 'auto',
+        resizeMode: 'contain'
+    },
+
+    helperText: {
+        color: Colors.light.tint,
+        textAlign: 'center',
+        marginBottom: 8
+    },
+
+    roamImageText: {
+        color: Colors.light.tint,
+        textAlign: 'center',
+        marginTop: 10,
+        fontWeight: 700,
+        fontSize: 36
+    },
+
+    students: {
+        marginTop: 18
+    },
+
+    nonStudents: {
+        marginTop: 36
+    },
+
+    phoneNumberInput: {
+        backgroundColor: 'white',
+        padding: 4,
+    }
+})
